343a16f21e2ecfae700a3b28ddced2d4c405590e
fcnaud <dcf_xyz@outlook.com>
2021-12-26 17:51:34 +0800

    config : update

--- lua/basic.lua
+++ lua/basic.lua
@@ -8,17 +8,18 @@ vim.o.swapfile = false
 vim.wo.number = true
 vim.wo.relativenumber = true
 vim.wo.cursorline = true
+vim.wo.cursorcolumn = true
 
 vim.wo.signcolumn = "yes"
 vim.wo.colorcolumn = "80"
 
 vim.o.tabstop = 4
-vim.bo.tabstop = 2
+vim.bo.tabstop = 4
 vim.o.softtabstop = 4
 vim.o.shiftround = true
 
 vim.o.shiftwidth = 4
-vim.bo.shiftwidth = 2
+vim.bo.shiftwidth = 4
 
 vim.o.expandtab = true
 vim.bo.expandtab = true
@@ -27,6 +28,14 @@ vim.bo.autoindent = true
 vim.o.smartindent = true
 
 vim.o.cmdheight = 2
+
+vim.o.timeoutlen = 100
+
+vim.o.splitbelow = true
+vim.o.splitright = true
+
+vim.o.autoread = true
+vim.bo.autoread = true
 
 vim.o.list = true
 vim.o.listchars = "space:·"
