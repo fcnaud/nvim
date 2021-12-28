741fb83eb3732f0727692e5e7950cd9f8726f40b
fcnaud <dcf_xyz@outlook.com>
2021-12-28 20:31:12 +0800

    plugin : change gitsigns setting

--- lua/plugin-config/gitsigns.lua
+++ lua/plugin-config/gitsigns.lua
@@ -2,7 +2,7 @@ require('gitsigns').setup {
   signs = {
     add = {hl = 'GitGutterAdd', text = '▋'},
     change = {hl = 'GitGutterChange',text= '▋'},
-    delete = {hl= 'GitGutterDelete', text = '▋'},
+    delete = {hl= 'GitGutterDelete', text = '▁'},
     topdelete = {hl ='GitGutterDeleteChange',text = '▔'},
     changedelete = {hl = 'GitGutterChange', text = '▎'},
   },
@@ -11,14 +11,19 @@ require('gitsigns').setup {
     noremap = true,
     buffer = true,
 
-    ['n ]g'] = { expr = true, "&diff ? ']g' : '<cmd>lua require\"gitsigns\".next_hunk()<CR>'"},
-    ['n [g'] = { expr = true, "&diff ? '[g' : '<cmd>lua require\"gitsigns\".prev_hunk()<CR>'"},
+    ['n ]c'] = { expr = true, "&diff ? ']c' : '<cmd>Gitsigns next_hunk<CR>'"},
+    ['n [c'] = { expr = true, "&diff ? '[c' : '<cmd>Gitsigns prev_hunk<CR>'"},
 
-    ['n <leader>hs'] = '<cmd>lua require"gitsigns".stage_hunk()<CR>',
-    ['n <leader>hu'] = '<cmd>lua require"gitsigns".undo_stage_hunk()<CR>',
-    ['n <leader>hr'] = '<cmd>lua require"gitsigns".reset_hunk()<CR>',
-    ['n <leader>hp'] = '<cmd>lua require"gitsigns".preview_hunk()<CR>',
-    ['n <leader>hb'] = '<cmd>lua require"gitsigns".blame_line()<CR>',
+    ['n <leader>hs'] = '<cmd>Gitsigns stage_hunk<CR>',
+    ['v <leader>hs'] = ':Gitsigns stage_hunk<CR>',
+    ['n <leader>hu'] = '<cmd>Gitsigns undo_stage_hunk<CR>',
+    ['n <leader>hr'] = '<cmd>Gitsigns reset_hunk<CR>',
+    ['v <leader>hr'] = ':Gitsigns reset_hunk<CR>',
+    ['n <leader>hR'] = '<cmd>Gitsigns reset_buffer<CR>',
+    ['n <leader>hp'] = '<cmd>Gitsigns preview_hunk<CR>',
+    ['n <leader>hb'] = '<cmd>lua require"gitsigns".blame_line{full=true}<CR>',
+    ['n <leader>hS'] = '<cmd>Gitsigns stage_buffer<CR>',
+    ['n <leader>hU'] = '<cmd>Gitsigns reset_buffer_index<CR>',
 
     -- Text objects
     ['o ih'] = ':<C-U>lua require"gitsigns".text_object()<CR>',
